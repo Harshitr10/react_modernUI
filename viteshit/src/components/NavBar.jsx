f0a4d0181a88035f26530d9d1e3be967b9ec2608
Harshitr10 <1012harshit@gmail.com>
2023-05-18 23:56:30 +0530

    added new files

---
+++ viteshit/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+
+
+const NavBar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between
+    items-center navbar">
+        <img src={logo} alt="hoobank"
+        className=""/>
+        
+
+    </nav>
+      )
+}
+
+export default NavBar
